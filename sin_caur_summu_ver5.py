5014cb56ec2a772380400052886f036f35959d35
user <chitatyan@mail.ru>
2018-11-07 09:38:06 +0200

    20181107_9_37

---
+++ sin_caur_summu_ver5.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from math import sin
+x = 1. * input("Lietotāj, lūdzu ievadi argumentu (x): ")
+
+y = sin(x)
+print("sin(%.2f) = %6.2f"%(x,y))
+
+k = 0
+a = (-1)**0*x**1/(1)
+S = a
+print "a0 = %6.2f S0 = %6.2f"%(a,S)
+
+#k = 1
+k = k + 1
+a = a * (-1)*x*x/((2*k)*(2*k+1))
+S = S + a
+print "a%d = %6.2f S%d = %6.2f"%(k,a,k,S)
+
+#k = 2
+k = k + 1
+a = a * (-1)*x*x/((2*k)*(2*k+1))
+S = S + a
+print "a%d = %6.2f S%d = %6.2f"%(k,a,k,S)
+
+#k = 3
+k = k + 1
+a = a * (-1)*x*x/((2*k)*(2*k+1))
+S = S + a
+print "a%d = %6.2f S%d = %6.2f"%(k,a,k,S)
