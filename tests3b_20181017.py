c710da7af158e3fb7708cf48941a732b1df2728e
yuliasemch <42993007+yuliasemch@users.noreply.github.com>
2019-01-02 01:33:48 +0200

    Update tests3b_20181017.py

--- tests3b_20181017.py
+++ tests3b_20181017.py
@@ -1,6 +1,9 @@
-while True:
-    line = input('>')
-    if line == 'done':
-        break
-    print(line)
-print('Done!')
+smallest = None
+print('Before')
+for value in [9, 41, 12, 3, 74, 15]:
+    if smallest is None:
+        smallest = value
+    elif value < smallest:
+        smallest=value
+    print(smallest, value)
+print('After', smallest)
