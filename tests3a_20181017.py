26f95f492e2f07e5fed0e8a1733d85bb517eb53b
yuliasemch <42993007+yuliasemch@users.noreply.github.com>
2019-01-02 01:32:42 +0200

    Update tests3a_20181017.py

--- tests3a_20181017.py
+++ tests3a_20181017.py
@@ -1,5 +1,10 @@
-n = 0
-while n > 0:
-    print('Lather')
-    print('Rinse')
-print('Dry off!')
+smallest = None
+print('Before')
+for value in [3, 41, 12, 9, 74, 15]:
+    if smallest is None:
+        smallest = value
+    elif value < smallest:
+        smallest=value
+    print(smallest, value)
+    
+print('After', smallest)
